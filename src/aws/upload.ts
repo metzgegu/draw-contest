64d8afa6e53592a7364ad27e70c5557ae0a0190c
metzgegu <guillaume.metzger@swile.co>
2023-03-31 00:41:54 +0200

    feat: add fileFilter to multer

--- src/aws/upload.ts
+++ src/aws/upload.ts
@@ -5,7 +5,16 @@ import multer from 'multer'
 import multerS3 from 'multer-s3'
 import { clientS3 } from './clientS3'
 
+const FILE_TYPE_ACCEPTED = ['image/jpeg', 'image/png']
+
 const upload = multer({
+  fileFilter: (_, file, cb) => {
+    if (!FILE_TYPE_ACCEPTED.includes(file.mimetype as string)) {
+      cb(new Error('File mimetype is not supported'))
+    } else {
+      cb(null, true)
+    }
+  },
   storage: multerS3({
     s3: clientS3,
     bucket: 'mytestbucket',
